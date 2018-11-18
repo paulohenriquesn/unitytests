34a9d9c083c0bfee07f6fedf975f515974361122
Paulo Henrique <gladgamer01@gmail.com>
2018-11-18 07:20:41 -0200

    FINISH.

---
+++ galaxyShooter/Scripts/PlayerAnimation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+    private Animator _anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _anim.SetBool("left",true);
+            _anim.SetBool("right", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _anim.SetBool("left", false);
+            _anim.SetBool("right", true);
+        }
+
+        if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            _anim.SetBool("left", false);
+            _anim.SetBool("right", false);
+        }
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            _anim.SetBool("left", false);
+            _anim.SetBool("right", false);
+        }
+
+
+
+
+    }
+}
